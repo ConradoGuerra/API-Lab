f3389844be2c6ddb20c1b1d1b6710c35447269bc
Conrado Guerra <conradoguerra@gmail.com>
2022-01-20 11:02:01 -0300

    Ensure to create a laboratory

--- __tests__/unity/laboratory/create-laboratory.spec.ts
+++ __tests__/unity/laboratory/create-laboratory.spec.ts
@@ -1,11 +1,18 @@
 class Laboratory {
-  name: string
-  constructor (name: string) {
-    this.name = name
-  }
+    public readonly name: string
+    public readonly address: string
+    public readonly status: number
+
+    constructor (name: string, address: string, status: number) {
+      this.name = name
+      this.address = address
+      this.status = status
+    }
 }
 
-it('should return a laboratory name', () => {
-  const laboratory = new Laboratory('any_name')
-  expect(laboratory.name).toBe('any_name')
+it('should return an error if laboratorys name is missing', () => {
+  const laboratory = new Laboratory('any_lab', 'any_address', 1)
+  expect(laboratory.name).toBe('any_lab')
+  expect(laboratory.address).toBe('any_address')
+  expect(laboratory.status).toBe(1)
 })
