44d90a59bbdf081a1fbcd9400626d2a8e111b932
Conrado Guerra <conradoguerra@gmail.com>
2022-01-21 15:45:22 -0300

    ensure in the test to create a laboratory

--- __tests__/unity/laboratory/create-laboratory.spec.ts
+++ __tests__/unity/laboratory/create-laboratory.spec.ts
@@ -1,13 +1,10 @@
-// // import { Laboratory } from '../../../src/entities/laboratory'
-// // import { LaboratoryData } from '../../../src/entities/laboratory-data'
+import { Laboratory } from '../../../src/entities/laboratory'
 
-// // class makeLabSpy extends Laboratory {
-
-// // }
-
-// it('should return the correct proporties of the laboratory created', () => {
-//   const laboratory = new Laboratory('any_lab', 'any', 1)
-//   expect(laboratory.name).toBe('any_lab')
-//   expect(laboratory.address).toBe('any_address')
-//   expect(laboratory.status).toBe(1)
-// })
+describe('Create Laboratory', () => {
+  it('should return a created laboratory when its instatiated', () => {
+    const laboratory = new Laboratory('any_lab', 'any_address', 1)
+    expect(laboratory.name).toBe('any_lab')
+    expect(laboratory.address).toBe('any_address')
+    expect(laboratory.status).toBe(1)
+  })
+})
