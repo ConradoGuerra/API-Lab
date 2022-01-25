7b920ebb6b3532a68e921a902c6e7cfb988f594c
Conrado Guerra <conradoguerra@gmail.com>
2022-01-25 20:08:54 -0300

    Creating a container

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,6 +4,6 @@ import { sequelize } from './repositories/helpers/sqlite-helper'
 sequelize
   .sync()
   .then((result) => {
-    app.listen(process.env.PORT || 8080, () => console.log('Server online!'))
+    app.listen(process.env.PORT || 80, () => console.log('Server online!'))
   })
   .catch((err) => console.log(err))
